253446cbaf70bbb2765f65c0e33d5436ed9e4748
xn012557 <lichunlin@xiaoniu66.com>
2018-10-16 10:23:56 +0800

    update code and doc

--- src/main/java/com/study/java/hash/HashCalcTest.java
+++ src/main/java/com/study/java/hash/HashCalcTest.java
@@ -6,8 +6,10 @@
  */
 package com.study.java.hash;
 
+import java.util.concurrent.ConcurrentHashMap;
+
 /**
- * 功能描述: XXXX
+ * 功能描述: Hash计算测试
  * <p/>
  * 创建人: chunlin.li
  * <p/>
@@ -15,5 +17,81 @@ package com.study.java.hash;
  * <p/>
  * Copyright (c) 深圳市牛鼎丰科技有限公司-版权所有
  */
-public class HashCalcTest {
+public class HashCalcTest extends ConcurrentHashMap {
+    private transient final int hashSeed = randomHashSeed(this);
+
+    static final int MAXIMUM_CAPACITY = 1 << 30;
+
+    private static int randomHashSeed(ConcurrentHashMap instance) {
+        if (sun.misc.VM.isBooted() && Holder.ALTERNATIVE_HASHING) {
+            return sun.misc.Hashing.randomHashSeed(instance);
+        }
+
+        return 0;
+    }
+
+    private static class Holder {
+
+        /**
+         * Enable alternative hashing of String keys?
+         *
+         * <p>Unlike the other hash map implementations we do not implement a
+         * threshold for regulating whether alternative hashing is used for
+         * String keys. Alternative hashing is either enabled for all instances
+         * or disabled for all instances.
+         */
+        static final boolean ALTERNATIVE_HASHING;
+
+        static {
+            // Use the "threshold" system property even though our threshold
+            // behaviour is "ON" or "OFF".
+            String altThreshold = java.security.AccessController.doPrivileged(
+                    new sun.security.action.GetPropertyAction(
+                            "jdk.map.althashing.threshold"));
+
+            int threshold;
+            try {
+                threshold = (null != altThreshold)
+                        ? Integer.parseInt(altThreshold)
+                        : Integer.MAX_VALUE;
+
+                // disable alternative hashing if -1
+                if (threshold == -1) {
+                    threshold = Integer.MAX_VALUE;
+                }
+
+                if (threshold < 0) {
+                    throw new IllegalArgumentException("value must be positive integer.");
+                }
+            } catch(IllegalArgumentException failed) {
+                throw new Error("Illegal value for 'jdk.map.althashing.threshold'", failed);
+            }
+            ALTERNATIVE_HASHING = threshold <= MAXIMUM_CAPACITY;
+        }
+    }
+
+    private int hash(Object k) {
+        int h = hashSeed;
+
+        if ((0 != h) && (k instanceof String)) {
+            return sun.misc.Hashing.stringHash32((String) k);
+        }
+
+        h ^= k.hashCode();
+
+        // Spread bits to regularize both segment and index locations,
+        // using variant of single-word Wang/Jenkins hash.
+        h += (h <<  15) ^ 0xffffcd7d;
+        h ^= (h >>> 10);
+        h += (h <<   3);
+        h ^= (h >>>  6);
+        h += (h <<   2) + (h << 14);
+        return h ^ (h >>> 16);
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(new HashCalcTest().hash(new String("3242hytt234")));
+    }
+
 }
